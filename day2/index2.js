2030846af0537db4a41238d6ae87db9161cf5768
uwiduhaye1 <joseuwiduhayee@gmail.com>
2023-11-22 06:06:46 -0800

    update

---
+++ day2/index2.js
@@ -0,0 +1,65 @@
+// let variable //
+
+
+let num = 1;
+num = 2;
+console.log(num);
+
+ let num1;
+  num1=9;
+
+ console.log(num);
+
+ let a=9;
+ let b=10;
+
+ console.log(b);
+
+
+ let num2=10;
+ let num3=40;
+ console.log(num3);
+
+ let numone=5;
+ numtwo=numone;
+ console.log(numtwo);
+ 
+let numOne = 30;
+let numTwo= numOne;
+console.log(numTwo);
+
+//variable const//
+
+
+const num6 = 100;
+console.log(num6);
+
+
+const c = 5;
+console.log(c);
+
+const num4 = 8;
+console.log('The value of num is: ' + num4);
+
+
+const 
+numg = 2;
+console.log(numg);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
