ff62e5fdc605be72853d4d23c9c28d5f6e109939
uwiduhaye1 <joseuwiduhayee@gmail.com>
2023-11-24 05:00:24 -0800

    update3

--- day1/index.js
+++ day1/index.js
@@ -25,6 +25,8 @@ var myFirstName = "Beau";
 var myLastName = "Carnes";
 //Understanding Uninitialized Variables//
 
+
+
 var a = 5;
 var b = 10;
 var c = "I am a";
---
+++ day4/index4.js
@@ -0,0 +1,45 @@
+
+//Connect Javascript strings//
+
+const textOne = 'Hello';
+const textTwo = 'World';
+const combined = textOne + ' ' + textTwo;
+console.log(combined);
+
+//if statement - add missing condition//
+
+let text = 'hello';
+if (text) {
+   text = text + ' world';
+}
+console.log(text === 'hello world');
+
+
+//function//
+
+
+function logMessageOne() {
+    console.log('How are you?');
+  }
+   
+  const logMessageTwo = function() {
+    console.log('Great, thanks!');
+  }
+   
+  logMessageOne();
+   
+  logMessageTwo();
+
+
+  function func() {
+    return 5;
+  }
+   
+  const result = func();
+   
+  console.log(result);
+
+  //function declaration//
+
+  
+   
