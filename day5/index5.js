f734561c944acc000c49962cf859b7ff66b7ba38
uwiduhaye1 <joseuwiduhayee@gmail.com>
2023-11-28 05:22:35 -0800

    update

---
+++ day5/index5.js
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">
+  <title>Tailwind Header</title>
+</head>
+<body class="bg-gray-100">
+
+  <header class="bg-blue-500 text-white p-4">
+    <div class="container mx-auto">
+      <div class="flex items-center justify-between">
+        <div class="text-2xl font-semibold">Logo</div>
+
+        <nav class="space-x-4">
+          <a href="#" class="hover:text-gray-300">Home</a>
+          <a href="#" class="hover:text-gray-300">About</a>
+          <a href="#" class="hover:text-gray-300">Services</a>
+          <a href="#" class="hover:text-gray-300">Contact</a>
+        </nav>
+      </div>
+    </div>
+  </header>
+
+
+
+</body>
+</html>
