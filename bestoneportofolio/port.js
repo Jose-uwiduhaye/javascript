141d2005a9e7355277f2cf950a9f97c030d96513
uwiduhaye1 <joseuwiduhayee@gmail.com>
2023-12-22 12:06:27 +0200

    update

--- bestoneportofolio/port.js
+++ bestoneportofolio/port.js
@@ -1,15 +1,23 @@
+
+let menuIcon=document.querySelector('#menu-icon');
+let navbar=document.querySelector('.navbar');
+menuIcon.onclick=()=>{
+    menuIcon.classList.toggle('bx-x');
+    varbar.classList.toggle('active');
+};
+
 let  sections= document.querySelectorAll('section');
 let navlinks=document.querySelectorAll('header nav a');
 window.onscroll=()=>{
     sections.forEach(sec=>{
-        let stop=window.scrollY;
+        let top=window.scrollY;
         let offset=sec.offsetTop -150;
         let height=sec.offsetHeight;
         let id=sec.getAttributr('id');
         if(top>=offset&&top<offset+height){
             navlinks.forEach(links=>{
                 links.classList.remove('active');
-                // document.querySelector('header nav a')
+                document.querySelector('header nav a[href*=' +']').classList.add('active');
             });
         }
     });
